0c9798ddfdfd30d46fe4b2b4cf7accee0125187b
future-zjj <1805862661@qq.com>
2022-02-04 01:17:14 +0800

    Callable

--- ThreadLocal/src/main/java/com/zjj/thread/concurrency/Fibonacci.java
+++ ThreadLocal/src/main/java/com/zjj/thread/concurrency/Fibonacci.java
@@ -1,4 +1,29 @@
 package com.zjj.thread.concurrency;
 
-public class Fibonacci {
+import com.zjj.thread.net.mindview.util.Generator;
+
+import java.util.Arrays;
+
+public class Fibonacci implements Generator<Integer>,Runnable {
+    private int count;
+    private final int n;
+    public Fibonacci(int n) { this.n = n; }
+
+    @Override
+    public Integer next() {
+        return fib(count++);
+    }
+    private int fib(int n) {
+        if (n < 2) return 1;
+        return fib(n - 2) + fib(n - 1);
+    }
+
+    @Override
+    public void run() {
+        Integer[] sequence = new Integer[n];
+        for(int i = 0; i < n; i++)
+            sequence[i] = next();
+        System.out.println(
+                "Seq. of " + n + ": " + Arrays.toString(sequence));
+    }
 }
--- ThreadLocal/src/main/java/com/zjj/thread/concurrency/TaskWithResult.java
+++ ThreadLocal/src/main/java/com/zjj/thread/concurrency/TaskWithResult.java
@@ -1,2 +1,16 @@
-package com.zjj.thread.concurrency;public class TaskWithResult {
+package com.zjj.thread.concurrency;
+
+import java.util.concurrent.Callable;
+
+public class TaskWithResult implements Callable<String> {
+
+    private int id;
+    public TaskWithResult(int id){
+        this.id=id;
+    }
+
+    @Override
+    public String call() throws Exception {
+        return "result of TaskWithResult "+id;
+    }
 }
